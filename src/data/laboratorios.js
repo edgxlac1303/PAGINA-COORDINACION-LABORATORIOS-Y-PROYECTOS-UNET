8817315bef1dfb365dd76d497879a607c6341754
edgxlac1303 <edgxlac1303@gmail.com>
2026-08-16 14:09:31 -0400

    Paginacion labs

---
+++ src/data/laboratorios.js
@@ -0,0 +1,450 @@
+export const laboratorios = [
+    {
+        item: 1,
+        nombre: "BIOQUÍMICA",
+        departamento: "QUÍMICA",
+        ubicacion: "3 ER NIVEL EDIFICIO 12",
+        aula: "12C13"
+    },
+    {
+        item: 2,
+        nombre: "QUÍMICA ANALÍTICA",
+        departamento: "QUÍMICA",
+        ubicacion: "EDIFICIO 12",
+        aula: "12C13"
+    },
+    {
+        item: 3,
+        nombre: "QUÍMICA GENERAL I (071)",
+        departamento: "QUÍMICA",
+        ubicacion: "EDIFICIO 33",
+        aula: "NO POSEE"
+    },
+    {
+        item: 4,
+        nombre: "QUÍMICA GENERAL I (072)",
+        departamento: "QUÍMICA",
+        ubicacion: "EDIFICIO 33",
+        aula: "NO POSEE"
+    },
+    {
+        item: 5,
+        nombre: "QUÍMICA ORGÁNICA",
+        departamento: "QUÍMICA",
+        ubicacion: "EDIFICIO 33",
+        aula: "NO POSEE"
+    },
+    {
+        item: 6,
+        nombre: "QUÍMICA II",
+        departamento: "QUÍMICA",
+        ubicacion: "EDIFICIO 33",
+        aula: "NO POSEE"
+    },
+    {
+        item: 7,
+        nombre: "SISTEMA DE COMUNICACIONES E INGENIERÍA DE REDES",
+        departamento: "INGENIERÍA ELECTRÓNICA",
+        ubicacion: "2 DO NIVEL EDIFICIO 11",
+        aula: "11C9"
+    },
+    {
+        item: 8,
+        nombre: "INSTRUMENTACIÓN Y CONTROL",
+        departamento: "INGENIERÍA ELECTRÓNICA",
+        ubicacion: "1ER NIVEL EDIFICIO 11",
+        aula: "11B11"
+    },
+    {
+        item: 9,
+        nombre: "PROYECTO DE SISTEMAS CONTROL E INSTRUMENTACIÓN INDUSTRIAL",
+        departamento: "INGENIERÍA ELECTRÓNICA",
+        ubicacion: "1ER NIVEL EDIFICIO 11",
+        aula: "11B12"
+    },
+    {
+        item: 10,
+        nombre: "PROYECTO DE INSTRUMENTACIÓN ELECTRÓNICA",
+        departamento: "INGENIERÍA ELECTRÓNICA",
+        ubicacion: "1ER NIVEL EDIFICIO 11",
+        aula: "11B11"
+    },
+    {
+        item: 11,
+        nombre: "ELECTRÓNICA ANALÓGICA",
+        departamento: "INGENIERÍA ELECTRÓNICA",
+        ubicacion: "2 DO NIVEL EDIFICIO 11",
+        aula: "11C10"
+    },
+    {
+        item: 12,
+        nombre: "CIRCUITOS Y MEDICIONES",
+        departamento: "INGENIERÍA ELECTRÓNICA",
+        ubicacion: "2 DO NIVEL EDIFICIO 11",
+        aula: "11C12"
+    },
+    {
+        item: 13,
+        nombre: "SISTEMAS DIGITALES",
+        departamento: "INGENIERÍA ELECTRÓNICA",
+        ubicacion: "2 DO NIVEL EDIFICIO 11",
+        aula: "11C16"
+    },
+    {
+        item: 14,
+        nombre: "MICROPROCESADORES",
+        departamento: "INGENIERÍA ELECTRÓNICA",
+        ubicacion: "Por definir",
+        aula: "Por definir"
+    },
+    {
+        item: 15,
+        nombre: "INGENIERÍA ELÉCTRICA Y ELECTROTECNIA",
+        departamento: "INGENIERÍA ELECTRÓNICA",
+        ubicacion: "2 DO NIVEL EDIFICIO 11",
+        aula: "11B06"
+    },
+    {
+        item: 16,
+        nombre: "TECNOLOGÍA ELÉCTRICA",
+        departamento: "INGENIERÍA ELECTRÓNICA",
+        ubicacion: "1ER NIVEL EDIFICIO 11",
+        aula: "11B08"
+    },
+    {
+        item: 17,
+        nombre: "TECNOLOGÍA ELECTRÓNICA",
+        departamento: "INGENIERÍA ELECTRÓNICA",
+        ubicacion: "1ER PNIVEL EDIFICIO 11",
+        aula: "11B16"
+    },
+    {
+        item: 18,
+        nombre: "ELECTRÓNICA INDUSTRIAL (PROFEI)",
+        departamento: "INGENIERÍA ELECTRÓNICA",
+        ubicacion: "1ER NIVEL EDIFICIO 11",
+        aula: "11B17"
+    },
+    {
+        item: 19,
+        nombre: "RF Y MICROONDAS",
+        departamento: "INGENIERÍA ELECTRÓNICA",
+        ubicacion: "2 DO NIVEL EDIFICIO 11",
+        aula: "11C167"
+    },
+    {
+        item: 20,
+        nombre: "COMPUTACIÓN VALERIO WONG",
+        departamento: "INGENIERÍA INFORMÁTICA",
+        ubicacion: "PLANTA BAJA EDIFICIO 12",
+        aula: "12B4"
+    },
+    {
+        item: 21,
+        nombre: "SALA DE ARQUITECTURA",
+        departamento: "INGENIERÍA INFORMÁTICA",
+        ubicacion: "2 NIVEL EDIFICIO 12",
+        aula: "Por definir"
+    },
+    {
+        item: 22,
+        nombre: "COMPONENTES CONSTRUCTIVOS",
+        departamento: "ARQUITECTURA",
+        ubicacion: "EDIFICIO 31",
+        aula: "NO POSEE"
+    },
+    {
+        item: 23,
+        nombre: "FÍSICA I",
+        departamento: "MATEMÁTICA Y FÍSICA",
+        ubicacion: "EDIFICIO 33",
+        aula: "NO POSEE"
+    },
+    {
+        item: 24,
+        nombre: "FÍSICA II",
+        departamento: "MATEMÁTICA Y FÍSICA",
+        ubicacion: "EDIFICIO 33",
+        aula: "NO POSEE"
+    },
+    {
+        item: 25,
+        nombre: "RESISTENCIA DE MATERIALES",
+        departamento: "INGENIERÍA MECÁNICA",
+        ubicacion: "EDIFICIO 34",
+        aula: "NO POSEE"
+    },
+    {
+        item: 26,
+        nombre: "MATERIALES",
+        departamento: "INGENIERÍA MECÁNICA",
+        ubicacion: "SOTANO EDIFICIO 11",
+        aula: "11A8"
+    },
+    {
+        item: 27,
+        nombre: "MÁQUINAS Y HERRAMIENTAS",
+        departamento: "INGENIERÍA MECÁNICA",
+        ubicacion: "EDIFICIO 31",
+        aula: "NO POSEE"
+    },
+    {
+        item: 28,
+        nombre: "TERMODINÁMICA  II",
+        departamento: "INGENIERÍA MECÁNICA",
+        ubicacion: "EDIFICIO 31",
+        aula: "NO POSEE"
+    },
+    {
+        item: 29,
+        nombre: "PLANTA DE POTENCIA Y TURBOMÁQUINAS",
+        departamento: "INGENIERÍA MECÁNICA",
+        ubicacion: "EDIFICIO 31",
+        aula: "NO POSEE"
+    },
+    {
+        item: 30,
+        nombre: "CONFORMADO DE MATERIALES \"TEC.CÉSAR GONZÁLEZ\"",
+        departamento: "INGENIERÍA MECÁNICA",
+        ubicacion: "EDIFICIO 31",
+        aula: "NO POSEE"
+    },
+    {
+        item: 31,
+        nombre: "TRANSFERENCIA DE CALOR",
+        departamento: "INGENIERÍA MECÁNICA",
+        ubicacion: "EDIFICIO 11",
+        aula: "11A14"
+    },
+    {
+        item: 32,
+        nombre: "ENERGÍAS ALTERNATIVAS",
+        departamento: "INGENIERÍA MECÁNICA",
+        ubicacion: "EDIFICIO 31",
+        aula: "NO POSEE"
+    },
+    {
+        item: 33,
+        nombre: "DISEÑO MECÁNICO",
+        departamento: "INGENIERÍA MECÁNICA",
+        ubicacion: "2 DO NIVEL EDIFICIO 12",
+        aula: "12B9"
+    },
+    {
+        item: 34,
+        nombre: "MECÁNICA DE FLUIDOS",
+        departamento: "INGENIERÍA MECÁNICA",
+        ubicacion: "SOTANO EDIFICIO 11",
+        aula: "11A7"
+    },
+    {
+        item: 35,
+        nombre: "INGENIERÍA DE MÉTODOS \"HERNANDO CACERES\"",
+        departamento: "INGENIERÍA INDUSTRIAL",
+        ubicacion: "3ER NIVEL   EDIFICO 12",
+        aula: "12C19"
+    },
+    {
+        item: null,
+        nombre: "SIMULACIÓN",
+        departamento: "INGENIERÍA INDUSTRIAL",
+        ubicacion: "EDIFICIO B",
+        aula: "9 B"
+    },
+    {
+        item: 36,
+        nombre: "PAVIMENTOS",
+        departamento: "INGENIERÍA CIVIL",
+        ubicacion: "EDIFICIO 31",
+        aula: "NO POSEE"
+    },
+    {
+        item: 37,
+        nombre: "MATERIALES, SUELOS Y CONCRETOS",
+        departamento: "INGENIERÍA CIVIL",
+        ubicacion: "EDIFICIO 31",
+        aula: "NO POSEE"
+    },
+    {
+        item: 38,
+        nombre: "APICULTURA",
+        departamento: "INGENIERÍA DE PRODUCCION ANIMAL",
+        ubicacion: "TUQUERENA /RUBIO",
+        aula: "NO POSEE"
+    },
+    {
+        item: 39,
+        nombre: "ANATOMÍA ANIMAL",
+        departamento: "INGENIERÍA DE PRODUCCIÓN ANIMAL",
+        ubicacion: "EDIFICIO 31",
+        aula: "NO POSEE"
+    },
+    {
+        item: 40,
+        nombre: "MICROBIOLOGÍA GENERAL, APLICADA Y AMBIENTAL",
+        departamento: "INGENIERÍA DE PRODUCCIÓN ANIMAL",
+        ubicacion: "4TO NIVEL EDIFICIO 12",
+        aula: "12 D 11"
+    },
+    {
+        item: 41,
+        nombre: "FISIOLOGÍA ANIMAL",
+        departamento: "INGENIERÍA DE PRODUCCIÓN ANIMAL",
+        ubicacion: "EDIFICIO 31",
+        aula: "NO POSEE"
+    },
+    {
+        item: 42,
+        nombre: "SANIDAD ANIMAL",
+        departamento: "INGENIERÍA DE PRODUCCIÓN ANIMAL",
+        ubicacion: "EDIFICIO 31",
+        aula: "NO POSEE"
+    },
+    {
+        item: 43,
+        nombre: "PARASITOLOGÍA",
+        departamento: "INGENIERÍA DE PRODUCCIÓN ANIMAL",
+        ubicacion: "EDIFICIO 31",
+        aula: "NO POSEE"
+    },
+    {
+        item: 44,
+        nombre: "GENÉTICA",
+        departamento: "INGENIERÍA DE PRODUCCIÓN ANIMAL",
+        ubicacion: "3ER NIVEL   EDIFICIO 12",
+        aula: "12C08"
+    },
+    {
+        item: 45,
+        nombre: "INDUSTRIAS DE LA CARNE",
+        departamento: "INGENIERÍA DE PRODUCCIÓN ANIMAL",
+        ubicacion: "3ER NIVEL   EDIFICO 12",
+        aula: "12C03"
+    },
+    {
+        item: 46,
+        nombre: "INDUSTRIAS DE LA LECHE",
+        departamento: "INGENIERÍA DE PRODUCCIÓN ANIMAL",
+        ubicacion: "3ER NIVEL   EDIFICIO 12",
+        aula: "12C03"
+    },
+    {
+        item: 47,
+        nombre: "NUTRICIÓN ANIMAL",
+        departamento: "INGENIERÍA DE PRODUCCIÓN ANIMAL",
+        ubicacion: "3ER NIVEL   EDIFICIO 12",
+        aula: "12C09"
+    },
+    {
+        item: 48,
+        nombre: "ZOOLOGÍA AGRÍCOLA",
+        departamento: "INGENIERÍA DE PRODUCCIÓN ANIMAL",
+        ubicacion: "4TO NIVEL EDIFICIO 12",
+        aula: "12 D 26"
+    },
+    {
+        item: 50,
+        nombre: "BIOLOGÍA",
+        departamento: "INGENIERÍA DE PRODUCCIÓN ANIMAL",
+        ubicacion: "4TO NIVEL EDIFICIO 12",
+        aula: "12 D 26"
+    },
+    {
+        item: 51,
+        nombre: "AREA DE APOYO MICROBIOLÓGICO Y ESTERTILIZACIÓN \"AUTOCLAVE\"",
+        departamento: "INGENIERÍA DE PRODUCCIÓN ANIMAL",
+        ubicacion: "4TO NIVEL EDIFICIO 12",
+        aula: "12 D 16"
+    },
+    {
+        item: 53,
+        nombre: "INGENIERÍA AMBIENTAL",
+        departamento: "INGENIERÍA AMBIENTAL",
+        ubicacion: "EDIFICIO 31",
+        aula: "NO POSEE"
+    },
+    {
+        item: 54,
+        nombre: "PLANTA DE FRUTAS Y VEGETALES",
+        departamento: "INGENIERÍA AGROINDUSTRIAL",
+        ubicacion: "TUQUERENA /RUBIO",
+        aula: "NO POSEE"
+    },
+    {
+        item: 55,
+        nombre: "SUELOS",
+        departamento: "INGENIERÍA AGRONÓMICA",
+        ubicacion: "EDIFICIO 31",
+        aula: "NO POSEE"
+    },
+    {
+        item: 56,
+        nombre: "BOTÁNICA AGRÍCOLA",
+        departamento: "INGENIERÍA AGRONÓMICA",
+        ubicacion: "4TO NIVEL EDIFICIO 12",
+        aula: "12D23"
+    },
+    {
+        item: 58,
+        nombre: "CULTIVO DE TEJIDOS",
+        departamento: "INGENIERÍA AGRONÓMICA",
+        ubicacion: "4TO NIVEL EDIFICIO 12",
+        aula: "12D05"
+    },
+    {
+        item: 59,
+        nombre: "FISIOLOGÍA VEGETAL",
+        departamento: "INGENIERÍA AGRONÓMICA",
+        ubicacion: "4TO NIVEL EDIFICIO 12",
+        aula: "12D04"
+    },
+    {
+        item: 60,
+        nombre: "ENTOMOLOGÍA",
+        departamento: "INGENIERÍA AGRONÓMICA",
+        ubicacion: "3ER NIVEL EDIFICIO 12",
+        aula: "12C04"
+    },
+    {
+        item: 61,
+        nombre: "FITOPATOLOGÍA",
+        departamento: "INGENIERÍA AGRONÓMICA",
+        ubicacion: "4TO NIVEL EDIFICIO 12",
+        aula: "12D14"
+    },
+    {
+        item: 62,
+        nombre: "AGROECOLOGÍA",
+        departamento: "INGENIERÍA AGRONÓMICA",
+        ubicacion: "EDIFICIO B DIAGONAL CAPILLA",
+        aula: "NO POSEE"
+    },
+    {
+        item: 63,
+        nombre: "FUNDAMENTOS DE HIDRÁULICA",
+        departamento: "INGENIERÍA AGRONÓMICA",
+        ubicacion: "Por definir",
+        aula: "Por definir"
+    },
+    {
+        item: 65,
+        nombre: "PROTECCIÓN VEGETAL",
+        departamento: "INGENIERÍA AGRONÓMICA",
+        ubicacion: "Por definir",
+        aula: "Por definir"
+    },
+    {
+        item: 66,
+        nombre: "MANEJO DE FERTILIDAD Y SUELOS",
+        departamento: "INGENIERÍA AGRONÓMICA",
+        ubicacion: "Por definir",
+        aula: "Por definir"
+    },
+    {
+        item: 67,
+        nombre: "FORRAJICULTURA",
+        departamento: "INGENIERÍA AGRONÓMICA",
+        ubicacion: "4TO NIVEL EDIFICIO 12",
+        aula: "12D24"
+    }
+];
